fdcf95022fa6fd36a2e09baec9c8e577616ded76
Maya Krishnan <mayark527@gmail.com>
2026-05-04 10:43:56 -0500

    Created PlaceDetails view for when a star in result's map is clicked

---
+++ Friensy/PlaceDetails.swift
@@ -0,0 +1,62 @@
+//
+//  PlaceDetails.swift
+//  Friensy
+//
+//  Created by Maya Krishnan on 5/4/26.
+//
+
+import SwiftUI
+import MapKit
+
+struct PlaceDetails: View {
+    let mapItem: MKMapItem
+    var body: some View {
+        let placemark = mapItem.placemark
+        
+        let address =
+        (placemark.subThoroughfare ?? "") + " " +
+        (placemark.thoroughfare ?? "") + "\n" +
+        (placemark.locality ?? "") + ", " +
+        (placemark.administrativeArea ?? "") + " " +
+        (placemark.postalCode ?? "")
+        
+        VStack(spacing: 20) {
+            
+            Text(mapItem.name ?? "Unknown Place")
+                .font(.title)
+                .bold()
+            
+            Text(address)
+                .multilineTextAlignment(.center)
+            
+            if let phone = mapItem.phoneNumber {
+                Text(phone)
+            }
+            
+            if let url = mapItem.url {
+                Link("Website", destination: url)
+            }
+            
+            Button("Open in Maps") {
+                let coordinate = placemark.coordinate
+                let url = URL(string: "maps://?daddr=\(coordinate.latitude),\(coordinate.longitude)")
+                
+                if let url = url {
+                    UIApplication.shared.open(url)
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    let mockItem = MKMapItem(placemark: MKPlacemark(
+        coordinate: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
+    ))
+    mockItem.name = "Sample Place"
+    
+    return PlaceDetails(mapItem: mockItem)
+}
