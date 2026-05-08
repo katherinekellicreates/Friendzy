26556b9b85aef3e36385b7bce92d5ea07549ac3c
Katherine Kelli <katherinekelli.creates@gmail.com>
2026-05-08 10:57:00 -0500

    changes result message to match how many items are counted

--- Friensy/Results.swift
+++ Friensy/Results.swift
@@ -21,9 +21,17 @@ struct Results: View {
                 Text("Your Ideas:")
                     .foregroundStyle(.white)
                     .font(Font.custom("BPreplay", size: 35))
-                Text("\(ideas.count) ways to spend your day!")
-                       .foregroundStyle(.white.opacity(0.8))
-                       .font(Font.custom("BPreplay", size: 20))
+            
+                if !ideas.isEmpty {
+                    Text(
+                        ideas.count == 1
+                        ? "1 way to spend your day!"
+                        :"\(ideas.count) ways to spend your day!"
+                    )
+                    .foregroundStyle(.white.opacity(0.8))
+                    .font(Font.custom("BPreplay", size: 20))
+                }
+                
                 if ideas.isEmpty {
                     Text("No matches — try different settings")
                         .foregroundStyle(.white.opacity(0.8))
